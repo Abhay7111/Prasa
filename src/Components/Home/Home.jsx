d6d3dac869b47062c71b67655536b3c7d7c4e7a4
Abhay Vishwakarma <gaamaofficial@gmail.com>
2024-09-06 08:25:50 +0530

    second screen updted

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,15 +4,16 @@ import Lstr from '../dgn/Lstr'
 
 function Home() {
   return (
-    <div className='w-full overflow-x-hidden flex flex-col h-fit sm:flex-row items-start justify-between px-5'>
+    <div className=''>
+        <div className='w-full overflow-x-hidden flex flex-col h-fit sm:flex-row items-start justify-between px-5'>
         <div className='flex relative flex-col items-start justify-between calc-css-h-full pb-10'>
             <h1 className='w-full sm:w-[75%] uppercase relative z-10 font-semibold text-xl mt-10 sm:mt-0'>The heat, soul, and creativity behind the exceptional accessories at prasa</h1>
-            <div className='scale-[1.2] z-0 absolute sm:relative top-[400%] -right-[19%] sm:top-auto sm:right-auto overflow-hidden'>
+            <div className='scale-[1.2] z-0 absolute sm:relative top-[200%] -right-[19%] sm:top-auto sm:right-auto overflow-hidden'>
             <Lstr/>
             </div>
         </div>
-        <div className='xl:w-[68%] flex flex-col justify-end items-start z-20 relative calc-css-h-full'>
-            <div className='w-full max-h-full pt-32'>
+        <div className='xl:w-[68%] flex flex-col justify-end items-start z-20 relative h-fit calc-css-h-full'>
+            <div className='w-full max-h-full sm:pt-32'>
                 <p className='w-full flex items-center justify-between sm:py-0 py-10'>
                     <p className='opacity-80 sm:w-fit w-[65%]'>Shop now and find the perfect pieces to complete any outfit.</p>
                     <NavLink to={`/`}><li className='uppercase tracking-tighter text-sm font-medium list-disc'>shop now</li></NavLink>
@@ -20,7 +21,10 @@ function Home() {
                 <img src="https://cdn.pixabay.com/photo/2020/04/22/08/17/portrait-5076529_960_720.jpg" alt="Image not found" className='text-xl font-bold uppercase w-full h-auto sm:max-h-[90%] object-cover' />
             </div>
         </div>
+        </div>
+        <div className=' w-full min-h-screen'>
 
+        </div>
     </div>
   )
 }
